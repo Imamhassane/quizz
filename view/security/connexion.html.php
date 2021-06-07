8ce6ebe6975327e22dd8c8c5f304df2f4c256e59
Imam Hassane <imamniass123@gmail.com>
2021-06-07 09:39:28 +0000

    gestion de la pqge de connexion et require du menu dans inscription selon l'utilisateur connecté

--- view/security/connexion.html.php
+++ view/security/connexion.html.php
@@ -3,48 +3,9 @@ if (isset($_SESSION['arrayError'])){
     $arrayError = $_SESSION['arrayError'];
     unset($_SESSION['arrayError']);
 }
+
 require(ROUTE_DIR.'view/inc/header.inc.html.php');
 ?>
-<!-- 
-<div class="container">
-        <div class="row justify-content-center align-items-center" style="height:100vh; ">
-            <div class="col-6" >
-                <div class="card"  style="background-color:#FAFAFA;border-radius: 10px;" >
-                    <div class="card-body">
-                    <?php if (isset($arrayError['erreurConnexion'])):?>
-                        <div class="alert alert-danger" style="height:40px;margin-top:8%" role="alert">
-                            <?= isset($arrayError['erreurConnexion']) ? $arrayError['erreurConnexion'] : '' ; ?>
-                        </div>
-                    <?php endif ?>
-                        <form method="POST" action="<?=WEB_ROUTE?>">  
-                        <input type="hidden" name="controllers" value="security">
-                        <input type="hidden" name="action" value="connexion">
-                            <div class="form-group">
-                                <label for="">Login</label>
-                                <input type="text" class="form-control" name="login" style="background-color:#FAFAFA;border-radius: 4px;">
-                                <small class = "form-text text-danger">
-                                    <?= isset($arrayError['login']) ? $arrayError['login'] : '' ;?>
-                                </small>
-                            </div>
-                            <div class="form-group">
-                                <label for="">Mot de pass</label>
-                                <input type="password" class="form-control" name="password"style="background-color:#FAFAFA;border-radius: 4px;">
-                                <small class = "form-text text-danger">
-                                    <?= isset($arrayError['password']) ? $arrayError['password'] : '' ;?>
-                                </small>                                
-                            </div>
-                            <button type="submit" class="btn btn-primary" name="valider" style="border-radius: 4px;">Se connecter</button>
-
-                            <div style="float:right;margin-top:20px;color:#0069D9;text-decoration:none;">
-                                <span>Vous n'avez pas de compte ?</span></br>
-                                <a href="<?=WEB_ROUTE.'?controllers=security&view=inscription'?>" style="float:right;margin-top:0px"> S'inscrire</a> 
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>     -->
     <div class="container ">
         <div class="row">
             <div class="col" style="margin-top: 5%;">
@@ -60,7 +21,7 @@ require(ROUTE_DIR.'view/inc/header.inc.html.php');
                         <input type="hidden" name="action" value="connexion">
                     <div class="text-center  mb-4">
                             <h1>Formulaire de connexion</h1>
-                        </div>
+                    </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Email </label>
                                 <input type="email" class="form-control" name="login">
--- view/security/inscription.html.php
+++ view/security/inscription.html.php
@@ -4,95 +4,10 @@ if (isset($_SESSION['arrayError'])){
     unset($_SESSION['arrayError']);
 }
 require(ROUTE_DIR.'view/inc/header.inc.html.php');
+if (est_admin()){
+    require_once(ROUTE_DIR.'view/inc/menu.inc.html.php');
+}
 ?>
-<!-- 
-<div class="container">
-        <div class="row justify-content-center align-items-center" style="height:100vh">
-            <div class="col-6">
-                <div class="card"style="background-color:#FAFAFA;border-radius: 10px;" >
-                    <div class="card-body">
-                    <?php if (isset($arrayError['erreurConnexion'])):?>
-                        <div class="alert alert-danger" style="height:40px" role="alert">
-                            <?= isset($arrayError['erreurConnexion']) ? $arrayError['erreurConnexion'] : '' ; ?>
-                        </div>
-                    <?php endif ?>
-                        <form method="POST" action="<?=WEB_ROUTE?>">  
-                            <input type="hidden" name="controllers" value="security">
-                            <input type="hidden" name="action" value="inscription">
-                        <div class="row">
-                                <div class="col-md-6">
-                                    <label for="">Nom</label>
-                                    <input type="text" class="form-control" name="nom"style="background-color:#FAFAFA;border-radius: 4px;">
-                                    <small class = "form-text text-danger">
-                                        <?= isset($arrayError['nom']) ? $arrayError['nom'] : '' ;?>
-                                    </small>
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="">Prénom</label>
-                                    <input type="text" class="form-control" name="prenom"style="background-color:#FAFAFA;border-radius: 4px;">
-                                    <small class = "form-text text-danger">
-                                        <?= isset($arrayError['prenom']) ? $arrayError['prenom'] : '' ;?>
-                                    </small>
-                                </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-12 mt-3" >
-                                <label for="">Date de naissance</label>
-                                <input type="text" class="form-control" name="datenaiss"style="background-color:#FAFAFA;border-radius: 4px;">
-                                <small class = "form-text text-danger">
-                                    <?= isset($arrayError['datenaiss']) ? $arrayError['datenaiss'] : '' ;?>
-                                </small>                                
-                            </div>
-                        </div>
-                        <div class="mt-4 mb-4">
-                            <div class="form-check form-check-inline">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" style="" name="sexe" value="F" checked> Féminin
-                                </label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="sexe" value="M"> Masculin
-                                </label>
-                            </div>
-                            <small class = "form-text text-danger">
-                                    <?= isset($arrayError['sexe']) ? $arrayError['sexe'] : '' ;?>
-                            </small>   
-                        </div>
-                        <div class="form-group"  >
-                                <label for="">Login</label>
-                                <input type="text" class="form-control" name="login"style="background-color:#FAFAFA;border-radius: 4px;">
-                                <small class = "form-text text-danger">
-                                    <?= isset($arrayError['login']) ? $arrayError['login'] : '' ;?>
-                                </small>
-                        </div>
-                        <div class="form-group">
-                                <label for="">Password</label>
-                                <input type="password" class="form-control" name="password"style="background-color:#FAFAFA;border-radius: 4px;">
-                                <small class = "form-text text-danger">
-                                    <?= isset($arrayError['password']) ? $arrayError['password'] : '' ;?>
-                                </small>                                
-                        </div>
-                        <div class="form-group">
-                                <label for="">Confirm password</label>
-                                <input type="password" class="form-control" name="confirmpassword"style="background-color:#FAFAFA;border-radius: 4px;">
-                                <small class = "form-text text-danger">
-                                    <?= isset($arrayError['confirmpassword']) ? $arrayError['confirmpassword'] : '' ;?>
-                                </small>                                
-                            </div>
-                        
-                            <button type="submit" class="btn btn-primary" style="border-radius: 4px;">S'inscrire</button>
-                            
-                            <div style="float:right;margin-top:20px;color:#0069D9;text-decoration:none;">
-                                <a href="<?=WEB_ROUTE.'?controllers=security&view=connexion'?>" style="float:right;margin-top:0px"> Se connecter</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>     -->
 <div class="container">
     <div class="row">
         <div class="col">
@@ -112,7 +27,7 @@ require(ROUTE_DIR.'view/inc/header.inc.html.php');
                         <div class="form-row mt-3">
                             <div class="form-group col-md-6">
                                 <label for="inputEmail4">Nom</label>
-                                <input type="text" class="form-control" name="nom">
+                                <input type="text" class="form-control" name="nom" value="">
                                 <small class = "form-text text-danger">
                                     <?= isset($arrayError['nom']) ? $arrayError['nom'] : '' ;?>
                                 </small>
