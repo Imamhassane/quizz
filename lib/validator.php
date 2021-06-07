8717a83172c72e2ad787f09b95dcbf5a88dbb70f
Imam Hassane <imamniass123@gmail.com>
2021-06-07 12:42:14 +0000

    gestion des dates

--- lib/validator.php
+++ lib/validator.php
@@ -40,88 +40,54 @@ function validation_champ(string $valeur, string  $key,  &$arrayError){
         $arrayError[$key] = "Ce champ est obligatoire";
     }   
 }
-
-function separateur( string $date,string  $key, array &$arrayError){
-   $test = explode('/', $date); 
-  // var_dump(strlen($test[0]));
-    $cptjour = strlen($test[0]) ;
-    $cptmois  =  strlen($test[1])  ;
-    $cptannee  = strlen($test[2] );
- 
-    $jour = $test[0];
-    $mois = $test[1];
-    $annee = $test[2];
-
-
-    
-    if($cptjour !=2 ){
-        $arrayError[$key] = 'Le jour doit être 2 chiffres';
-    }elseif($cptjour =2 ){
-        if($cptmois !=2 ){
-            $arrayError[$key] = 'Le mois doit etre 2 chiffres';
-        }else if($cptmois =2){
-            if($mois < 1 || $mois > 12){
-                $arrayError[$key] = 'Le mois doit etre compris entre 1 et 12';
-            }
-        }
-        switch ($mois) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-            if ($jour > 1 || $jour > 31) {
-                $arrayError[$key] ='Le jour doit etre compris entre 1 et 31';
-            }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-            if ($jour < 1 || $jour > 30) {
-                $arrayError[$key] ='Le jour doit etre compris entre 1 et 30';
-            }
-                break;
-            case 2:
-                if ($annee%400==0 || ($annee%4==0 && $annee%100!=0)) {
-                    if ($jour < 1 || $jour > 29) {
-                        $arrayError[$key] ='Le jour doit etre compris entre 1 et 29';
-                    }
-                }else {
-                    if ($jour < 1 || $jour > 28) {
-                        $arrayError[$key] ='Le jour doit etre compris entre 1 et 28';
-                    }
-                }
-                break;
-                
-        }
-    }
-
-    if($cptmois !=2){
-            $arrayError[$key] = 'Le mois doit etre 2 chiffres';
-        }else{
-            if($mois < 1 || $mois > 12){
-                $arrayError[$key] = 'Le mois doit etre compris entre 1 et 12';
-            }
-        }
-    if($cptannee != 4){
-            $arrayError[$key] = 'L\'année doit etre 4 chiffres';
-        }else{
-            if ($annee < 1){
-                $arrayError[$key] = 'l\'année doit etre supérieur à 1';
-            }
-        }
-    if (!is_numeric($jour)){
-            $arrayError[$key] = 'La date doit être numérique';
-    
-        }
-  
-    if (est_vide($date)) {
-        $arrayError[$key] = "Ce champ est obligatoire";
-        } 
-        
+function verif_mois($mois): bool
+{
+    return $mois > 0 && $mois <= 12;
 }
+function is_annee_bissextile(int $annee): bool
+{
+    return $annee % 400 == 0 || ($annee % 4 == 0 && $annee % 100 != 0);
+}
+function valide_datenaiss( $datenaiss , $test):bool{ 
+            $test = explode('/', $datenaiss); 
+        if (verif_mois($test[1]) == true) {
+            switch ($test[1]) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return $test[0] > 0 && $test[0] <= 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return $test[0] > 0 && $test[0] <= 30;
+                    break;
+                case 2:
+                    if (is_annee_bissextile($test[2])) {
+                        return $test[0] > 0 && $test[0] < 30;
+                    } else {
+                        return $test[0] > 0 && $test[0] < 29;
+                    }
+                default:
+                    return false;
+            }
+        }else{
+            return false;
+        }
+}
+function validation_date_naissance($datenaiss,string $key,&$arrayError,$test):void{
+    $test = explode('/', $datenaiss); 
+    if (est_vide($datenaiss)) {
+        $arrayError[$key] = "Ce champ est obligatoire";
+    }elseif(!valide_datenaiss($datenaiss,$test)) {
+        $arrayError[$key] = "La date de naissance est invalide";
+    }
+}
+
 
 ?>
