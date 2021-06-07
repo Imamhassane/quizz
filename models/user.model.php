c18f019eb8782fafd53e495ba7b77034d7bad0af
Imam Hassane <imamniass123@gmail.com>
2021-06-07 09:58:58 +0000

    ajouter un utilisateur , login exist , find all users ,

--- models/user.model.php
+++ models/user.model.php
@@ -13,36 +13,43 @@
   return [];
 }*/
 
-// function find_all_users():array{
-//   $json=file_get_contents(FILE_USERS);
-//   // 2 convertir le json en tableau
-//   return json_decode($json,true);
-// }
-// function find_login_password(string $login, string $password){
-//     $arrayUser=find_all_users();
-//     foreach($arrayUser as $user){
-//         if ($user['login']==$login && $user['password']==$password) {
+function find_all_users(){
+  $json =file_get_contents(FILE_USERS);
+  // 2 convertir le json en tableau
+  return json_decode($json,true);
+}
+function find_login_password(string $login, string $password){
+    $arrayUser=find_all_users();
+    foreach($arrayUser as $user){
+        if ($user['login']==$login && $user['password']==$password) {
           
-//           return $user;
-//         }
-//     }
-//     return [];
-// }
-// function add_user(array $user){
-//   $json=file_get_contents(FILE_USERS);
-//   $arrayUser= json_decode($json,true);
-//   $arrayUser[]=$user;
-//   //convertir le tableau en json
-//   $json = json_decode($arrayUser);
-//   file_put_contents(FILE_USERS , $json);
-// }
-// function login_exist(string $login):bool{
-//     $arrayUser=find_all_users();
-//     foreach($arrayUser as $user){
-//         if ($user['login']==$login && $user['password']==$password) {
-//           return $user;
-//         }
-//     }
-//     return [];
-// }
+          return $user;
+        }
+    }
+    return [];
+}
+
+
+function add_user(array $user){
+  $json=file_get_contents(FILE_USERS);
+  $arrayUser= json_decode($json,true);
+  $arrayUser[]=$user;
+  //convertir le tableau en json
+  $json = json_encode($arrayUser);
+  file_put_contents(FILE_USERS , $json);
+}
+
+
+
+
+
+function login_exist(string $login):array{
+    $arrayUser=find_all_users();
+    foreach($arrayUser as $user){
+        if ($user['login']==$login) {
+          return $user;
+        }
+    }
+    return [];
+}
 ?>
