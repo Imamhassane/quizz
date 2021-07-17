54a0d92516850ac064ecc6c2604a381155546f15
bassbiteye <bassbiteye45@gmail.com>
2021-07-17 00:33:01 +0000

    fonctionnalités du joueur

--- config/constante.php
+++ config/constante.php
@@ -1,6 +1,6 @@
 <?php 
-//define("WEB_ROUTE" , "http://niassimamhassane.alwaysdata.net/");
-define("WEB_ROUTE" , "http://localhost:8000/");
+define("WEB_ROUTE" , "http://niassimamhassane.alwaysdata.net/");
+//define("WEB_ROUTE" , "http://localhost:8000/");
 define('ROUTE_DIR', str_replace ('public', '' , $_SERVER['DOCUMENT_ROOT']));
 define("FILE_USERS" , ROUTE_DIR.'data/user.data.json' );
 define("FILE_QUESTIONS" , ROUTE_DIR.'data/question.data.json' );
